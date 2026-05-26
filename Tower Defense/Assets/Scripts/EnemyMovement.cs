c786c90e0705794174fcc0af203443ed6b1901bb
Matthew Yang <matthew.yang@scsstudent.org>
2026-05-26 00:55:09 -0700

    feat: completed part 7 and 8

--- Tower Defense/Assets/Scripts/EnemyMovement.cs
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -2,19 +2,26 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-   [field: SerializeField]
+    [field: SerializeField]
+    public float Speed { get; private set; } = 1f;
 
-   public float Speed { get; private set; } = 1f;
+    [field: SerializeField]
+    public Waypoint Target { get; private set; }
    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = Target.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Speed, 0, 0) * Time.deltaTime;
+        if (Target == null) { return; }
+        transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, Time.deltaTime * Speed);
+        transform.LookAt(Target.transform.position);
+        float distance = Vector3.Distance(transform.position, Target.transform.position);
+        if (distance <= 0.1f)
+        {
+            Target = Target.Next;
+        }
     }
 }
---
+++ Tower Defense/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,15 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [field: SerializeField]
+    public Waypoint Next { get; private set; }
+
+    void OnDrawGizmos()
+    {
+        if (Next == null) { return; }
+        Handles.color = Color.red;
+        Handles.DrawLine(transform.position, Next.transform.position, 3f);
+    }
+}
